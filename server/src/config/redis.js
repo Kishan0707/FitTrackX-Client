bd77f53909c63ed5e128fb17cc95a119ac2ee73a
Kishan Ahir <kk57161@gmail.com>
2026-03-25 12:23:21 +0530

    fixed redis

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,8 +1,12 @@
 const { Redis } = require("@upstash/redis");
 
+const redisUrl = process.env.UPSTASH_REDIS_REST_URL || process.env.UPSTASH_REDIS_URL;
+const redisToken =
+  process.env.UPSTASH_REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_TOKEN;
+
 const redisClient = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: redisUrl,
+  token: redisToken,
 });
 
 module.exports = redisClient;
--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,26 @@ const app = require("./app");
 const connectDB = require("./config/db");
 const http = require("http");
 const { initializeSocket } = require("./config/socket");
+
+const PORT = Number(process.env.PORT) || 5000;
+
 connectDB();
 
 const server = http.createServer(app);
 initializeSocket(server);
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+
+server.on("error", (error) => {
+  if (error.code === "EADDRINUSE") {
+    console.error(
+      `Port ${PORT} is already in use. Stop the running process or change PORT in server/.env.`,
+    );
+    process.exit(1);
+  }
+
+  throw error;
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   console.log("Socket.io initialized");
 });
