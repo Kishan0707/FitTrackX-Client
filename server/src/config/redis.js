587972a67de9005e28405fa332eb785465b78401
Kishan Ahir <kk57161@gmail.com>
2026-03-25 11:38:55 +0530

    server added complate

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -2,10 +2,17 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    tls: true, // 🔥 MUST for Upstash
+  },
 });
 redisClient.on("connect", () => console.log("Redis Client Connected"));
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+  } catch (err) {
+    console.log("Redis not Connected");
+  }
 })();
 module.exports = redisClient;
