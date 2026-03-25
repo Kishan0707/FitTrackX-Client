3bb6ecb19d1346b79d32061d2f25c9892c02b93c
Kishan Ahir <kk57161@gmail.com>
2026-03-25 12:18:11 +0530

    fixed redis

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,6 +1,6 @@
 const { Redis } = require("@upstash/redis");
 
-const redisClient = createClient({
+const redisClient = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
@@ -8,4 +8,4 @@ const redisClient = createClient({
 redisClient.on("connect", () => console.log("✅ Redis Connected"));
 redisClient.on("error", (err) => console.log("❌ Redis Error", err));
 
-module.exports = { redisClient, connectRedis };
+module.exports = redisClient;
