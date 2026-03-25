42726b1492e2aef1552b58c200a8995282a5d50e
Kishan Ahir <kk57161@gmail.com>
2026-03-25 10:45:57 +0530

    Removed client folder

--- client/src/pages/admin/AdminWorkout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminWorkout = () => {
-  return <div></div>;
-};
-
-export default AdminWorkout;
--- client/src/pages/progress/AddMeasurement.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddMeasurement = () => {
-  return <div></div>;
-};
-
-export default AddMeasurement;
