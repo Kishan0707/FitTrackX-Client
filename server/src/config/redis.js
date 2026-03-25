7c172f26ed2edcdc94970282343f2b75183a749b
Kishan Ahir <kk57161@gmail.com>
2026-03-25 11:59:02 +0530

    fixed redis

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -2,14 +2,28 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    reconnectStrategy: (retries) => {
+      if (retries > 5) {
+        console.log("❌ Redis reconnect stopped");
+        return new Error("Retry limit reached");
+      }
+      return Math.min(retries * 200, 2000);
+    },
+  },
 });
-redisClient.on("connect", () => console.log("Redis Client Connected"));
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
-(async () => {
+
+redisClient.on("connect", () => console.log("✅ Redis Connected"));
+redisClient.on("error", (err) => console.log("❌ Redis Error", err));
+
+const connectRedis = async () => {
   try {
-    await redisClient.connect();
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+    }
   } catch (err) {
-    console.log("Redis not Connected");
+    console.log("❌ Redis not Connected");
   }
-})();
-module.exports = redisClient;
+};
+
+module.exports = { redisClient, connectRedis };
